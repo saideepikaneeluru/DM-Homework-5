0c0ca3ea09bf5b8a45cdea344d5714060c1c621a
saideepikaneeluru <158000517+saideepikaneeluru@users.noreply.github.com>
2024-04-15 23:02:58 -0400

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -176,13 +176,13 @@ def question8():
     answers = {}
 
     # type: eval_float
-    answers['(a) precision for C0'] = (p*100)/(p*1000)
+    answers['(a) precision for C0'] = "(p*100)/(p*1000)"
 
     # type: eval_float
-    answers['(a) recall for C0'] = P
+    answers['(a) recall for C0'] = "P"
 
     # type: eval_float
-    answers['(b) F-measure of C0'] = (2 * p/10) / (p + 1/10) 
+    answers['(b) F-measure of C0'] = "(2 * p/10) / (p + 1/10)" 
 
     # type: string
     # choices: ['yes', 'no', 'unknown']
